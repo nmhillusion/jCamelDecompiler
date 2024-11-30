9a0d62f012591e8b816fc6886293d65646a7b068
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-11-30 20:31:22 +0700

    caching for custom view's folder_icon

--- src/main/java/tech/nmhillusion/jCamelDecoderApp/gui/CustomFileView.java
+++ src/main/java/tech/nmhillusion/jCamelDecoderApp/gui/CustomFileView.java
@@ -2,6 +2,7 @@ package tech.nmhillusion.jCamelDecoderApp.gui;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileView;
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -14,14 +15,25 @@ import java.io.InputStream;
  */
 public class CustomFileView extends FileView {
 
+    private static ImageIcon FOLDER_ICON;
+
     @Override
     public Icon getIcon(File f) {
+        if (null != FOLDER_ICON) {
+            return FOLDER_ICON;
+        }
+
         try (final InputStream folderIcon = getClass().getClassLoader().getResourceAsStream("icon/folder.png")) {
             if (null == folderIcon) {
                 throw new FileNotFoundException("File not found: icon/folder.png");
             }
 
-            return new ImageIcon(folderIcon.readAllBytes());
+            final ImageIcon imageIcon = new ImageIcon(folderIcon.readAllBytes());
+            imageIcon.setImage(imageIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH));
+
+            FOLDER_ICON = imageIcon;
+
+            return FOLDER_ICON;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
