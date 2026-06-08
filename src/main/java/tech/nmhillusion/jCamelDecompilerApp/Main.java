9efcc1225943fe4ea2a1cf4d9abe2e8b57d13b93
nmhillusion <nguyenminhhieu.geek@gmail.com>
2026-06-08 21:53:21 +0700

    do exit after finish cli execution

--- src/main/java/tech/nmhillusion/jCamelDecompilerApp/Main.java
+++ src/main/java/tech/nmhillusion/jCamelDecompilerApp/Main.java
@@ -37,6 +37,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length > 0) {
             executeCLICommand(args);
+            System.exit(0);
             return;
         }
 
